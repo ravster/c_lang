3cdbf38e9de63a36b4abb32cc1b51941ccc6225d
Ravi Desai <ravi@ravidesai.com>
2019-05-26 16:05:33 -0400

    Users can pass in how many entries to parse from STDIN
    ... and what the moving-average window should be
    
    The program will also return helpful errnos now in the case of it
    receiving incorrect arguments.

--- closing_prices/closing_prices.c
+++ closing_prices/closing_prices.c
@@ -5,27 +5,36 @@
 
 /*
   gcc -o closing_prices closing_prices.c
-  sed -n "2,21p" sp500_5_years.csv | cut -f 5 -d ',' | ./closing_prices
+  sed -n "2,71p" sp500_5_years.csv | cut -f 5 -d ',' | ./closing_prices 30 7
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
 
 /*
   TODO:
-  - Parse into any number of lines
-  Probably easiest to push onto a linked-list during parsing and then
-  put it all into an array for faster lookup when doing calculations.
   - Parse into a struct so we can gather data on more fields
   Probably a good time to add CSV support?
   - Figure out why we can't check for errno after the strtod() call
 */
 int
 main(int argc, char **argv) {
+  // Check that we got a number for how many lines to analyze from STDIN
+  if (argc < 3) {
+	printf("How many lines to parse from STDIN, and what is the period of the moving-average?\n");
+	return EINVAL;
+  }
+
   char line[60]; // Read in max 60 chars per line
-  int max_data = 20; // Read only 20 lines
+  int max_data = atoi(argv[1]);
   double all_data[max_data];
-  int moving_average_window = 5;
+  int moving_average_window = atoi(argv[2]);
+
+  if (max_data == 0 || moving_average_window == 0) {
+	printf("\nOne of the args couldn't be converted to an int.\n");
+	return EINVAL;
+  }
 
   for(int i = 0; i < max_data; i++) {
 	double closing;
