799cee29a4bb84ff13249e1f4ac681d8d4223660
Ravi Desai <ravi@ravidesai.com>
2019-05-26 10:39:33 -0400

    Calculate 5 tick moving average
    - Hardcoded to 5 ticks
    - Hardcoded to only running over 20 lines
    
    TODO:
    - Parse into any number of lines
      Probably easiest to push onto a linked-list during parsing and then
      put it all into an array for faster lookup when doing calculations.
    - Parse into a struct so we can gather data on more fields
      Probably a good time to add CSV support?
    - Figure out why we can't check for errno after the strtod() call

--- closing_prices/closing_prices.c
+++ closing_prices/closing_prices.c
@@ -1,25 +1,56 @@
 /*
-STDIN: Closing prices in decimal form
-STDOUT: 10-day moving average of closing price
+  STDIN: Prices in decimal form
+  STDOUT: 5-day moving average of prices
 */
 
 /*
-gcc -o closing_prices closing_prices.c
-sed -n "2,300p" sp500_5_years.csv | cut -f 5 -d ',' | ./closing_prices
+  gcc -o closing_prices closing_prices.c
+  sed -n "2,21p" sp500_5_years.csv | cut -f 5 -d ',' | ./closing_prices
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 
+/*
+  TODO:
+  - Parse into any number of lines
+  Probably easiest to push onto a linked-list during parsing and then
+  put it all into an array for faster lookup when doing calculations.
+  - Parse into a struct so we can gather data on more fields
+  Probably a good time to add CSV support?
+  - Figure out why we can't check for errno after the strtod() call
+*/
 int
 main(int argc, char **argv) {
-  printf("z1\n");
-  /* Start small.  Read from STDIN, output to STDOUT */
+  char line[60]; // Read in max 60 chars per line
+  int max_data = 20; // Read only 20 lines
+  double all_data[max_data];
+  int moving_average_window = 5;
 
-  // Read in max 60 chars
-  char line[60];
-  printf("z2\n");
+  for(int i = 0; i < max_data; i++) {
+	double closing;
+	if(fgets(line, 60, stdin) == NULL){
+	  printf("\nBroke at %d", i);
+	  break;
+	}
 
-  while(fgets(line, 60, stdin) != NULL) {
-	printf("Output: %s\n", line);
+	closing = strtod(line, NULL); // Parse input string to double
+	/* Should check errno after above, but its always 0 even with obviously incorrect data.
+	   Not sure why.  Continuing for now. */
+
+	all_data[i] = closing;
+  }
+
+  for(int i = 0; i < max_data; i++) {
+	if(i < moving_average_window){
+	  printf("Array[%d] = %f\n", i, all_data[i]);
+	} else {
+	  double sum = 0;
+	  for(int j = i - moving_average_window; j < i; j++) {
+		sum += all_data[j];
+	  }
+	  sum = sum / moving_average_window;
+	  printf("Array[%d] = %f, %d-moving-average = %f\n", i, all_data[i], moving_average_window, sum);
+	}
   }
 }
