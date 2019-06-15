1b15b8e47e2d9633dbda779683640c48cd720e82
Ravi Desai <ravi@ravidesai.com>
2019-06-15 17:10:44 -0400

    Populate array with struct
    - Read file line by line
    - parse struct from each line
    - append struct into GArray

--- closing_prices/closing_prices.c
+++ closing_prices/closing_prices.c
@@ -1,69 +1,90 @@
-/*
-  STDIN: Prices in decimal form
-  STDOUT: 5-day moving average of prices
-*/
+// DOING :
+// Read from file
+// Populate struct
+// Append to GArray
 
 /*
-  gcc -o closing_prices closing_prices.c
-  cut -f 5 -d ',' sp500_5_years.csv | tail -n +2 | ./closing_prices 320 3
+  gcc -L/usr/lib/x86_64-linux-gnu -I/usr/include/glib-2.0 -I/usr/lib/x86_64-linux-gnu/glib-2.0/include -o closing_prices closing_prices.c -lglib-2.0
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <glib.h>
 
 typedef struct {
-  double open, close;
+  double open, high, low, close, volume;
 } tick;
+
+// Split string
+// Return populated tick struct
+tick parse_tick(char* line) {
+  char* delim = ",";
+  gchar** strings = g_strsplit(line, delim, 0);
+
+  tick tick1;
+  tick1.open = g_strtod(strings[1], NULL);
+  tick1.high = g_strtod(strings[2], NULL);
+  tick1.low = g_strtod(strings[3], NULL);
+  tick1.close = g_strtod(strings[4], NULL);
+
+  return tick1;
+}
 
 /*
   TODO:
-  - Parse into a struct so we can gather data on more fields
-  Probably a good time to add CSV support?
   - Figure out why we can't check for errno after the strtod() call
 */
 int
 main(int argc, char **argv) {
-  // Check that we got a number for how many lines to analyze from STDIN
-  if (argc < 3) {
-	printf("Usage: ./closing_prices NUM_LINES MOVING_AVERAGE_WINDOW \n");
-	return EINVAL;
+  FILE* fp = NULL;
+  char* filepath = "sp500_5_years.csv";
+
+  fp = fopen(filepath, "r");
+  if (fp == NULL) {
+	fprintf(stderr, "Cannot open file %s", filepath);
+	return 1;
   }
 
-  char line[60]; // Read in max 60 chars per line
-  int max_data = atoi(argv[1]);
-  double all_data[max_data];
-  int moving_average_window = atoi(argv[2]);
+  char* line = NULL;
+  size_t len = 100;
+  GArray* arr = g_array_new(FALSE, FALSE, sizeof(tick));
 
-  if (max_data == 0 || moving_average_window == 0) {
-	printf("\nOne of the args couldn't be converted to an int.\n");
-	return EINVAL;
+  // Read line from file, create tick struct, append tick to array
+  while (getline(&line, &len, fp) != -1) {
+	tick tick1 = parse_tick(line);
+
+	g_array_append_val(arr, tick1);
   }
 
-  for(int i = 0; i < max_data; i++) {
-	double closing;
-	if(fgets(line, 60, stdin) == NULL){
-	  printf("\nBroke at %d", i);
-	  break;
-	}
+  printf("size of array: %d\n", arr->len);
 
-	closing = strtod(line, NULL); // Parse input string to double
-	/* Should check errno after above, but its always 0 even with obviously incorrect data.
-	   Not sure why.  Continuing for now. */
+  /* int moving_average_window = 5; */
 
-	all_data[i] = closing;
-  }
+  /* for(int i = 0; i < max_data; i++) { */
+  /* 	double closing; */
+  /* 	if(fgets(line, 60, stdin) == NULL){ */
+  /* 	  printf("\nBroke at %d", i); */
+  /* 	  break; */
+  /* 	} */
 
-  for(int i = 0; i < max_data; i++) {
-	if(i < moving_average_window){
-	  printf("Array[%d] = %f\n", i, all_data[i]);
-	} else {
-	  double sum = 0;
-	  for(int j = i - moving_average_window; j < i; j++) {
-		sum += all_data[j];
-	  }
-	  sum = sum / moving_average_window;
-	  printf("Array[%d] = %f, %d-moving-average = %f\n", i, all_data[i], moving_average_window, sum);
-	}
-  }
+  /* 	closing = strtod(line, NULL); // Parse input string to double */
+  /* 	/\* Should check errno after above, but its always 0 even with obviously incorrect data. */
+  /* 	   Not sure why.  Continuing for now. *\/ */
+
+  /* 	all_data[i] = closing; */
+  /* } */
+
+  /* for(int i = 0; i < max_data; i++) { */
+  /* 	if(i < moving_average_window){ */
+  /* 	  printf("Array[%d] = %f\n", i, all_data[i]); */
+  /* 	} else { */
+  /* 	  double sum = 0; */
+  /* 	  for(int j = i - moving_average_window; j < i; j++) { */
+  /* 		sum += all_data[j]; */
+  /* 	  } */
+  /* 	  sum = sum / moving_average_window; */
+  /* 	  printf("Array[%d] = %f, %d-moving-average = %f\n", i, all_data[i], moving_average_window, sum); */
+  /* 	} */
+  /* } */
 }
